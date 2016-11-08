aa1d2cef795508004d5da6dd70807346bc997445
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-08 13:31:35 +0100

    Adjust SDK reference, add some API's, bump 1.1.0

--- iphone/Classes/ComWilliamrijksenOnesignalModule.h
+++ iphone/Classes/ComWilliamrijksenOnesignalModule.h
@@ -8,8 +8,13 @@
 #import "TiModule.h"
 #import <OneSignal/OneSignal.h>
 
-@interface ComWilliamrijksenOnesignalModule : TiModule
-{
-}
+@interface ComWilliamrijksenOnesignalModule : TiModule {}
+
+typedef void(^TagsResultHandler)(NSDictionary*, NSError*);
+
+- (void)sendTag:(id)args;
+- (void)deleteTag:(id)args;
+- (id)getTags:(id)value;
+- (void)setLogLevel:(id)value;
 
 @end
