1ae4e0b011b256ae9e3ed0c426f1573d1d8d6e7b
William Rijksen <williamrijksen@gmail.com>
2016-12-02 19:43:55 +0100

    reformat example file

--- example/app.js
+++ example/app.js
@@ -1,58 +1,61 @@
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+    backgroundColor: 'white'
 });
 
 var onesignal = require('com.williamrijksen.onesignal');
 
 var addTag = Ti.UI.createButton({
-  title:'Add a tag',
-  width:Ti.UI.FILL,
-  height:Ti.UI.FILL,
-  color:'black',
-  opacity:0.8,
-  backgroundColor:'transparent',
-  borderColor:'#4ee47f',
-  top:60
+    title: 'Add a tag',
+    width: Ti.UI.FILL,
+    height: Ti.UI.FILL,
+    color: 'black',
+    opacity: 0.8,
+    backgroundColor: 'transparent',
+    borderColor: '#4ee47f',
+    top: 60
 });
 
-addTag.addEventListener('click',function(e){
-   onesignal.sendTag({key:'tag1', value:true});
-   alert('Tag added');
+addTag.addEventListener('click', function(e) {
+    onesignal.sendTag({
+        key: 'tag1',
+        value: true
+    });
+    alert('Tag added');
 });
 win.add(addTag);
 
 
-onesignal.addEventListener("OneSignalNotificationOpened",function(evt){
-	alert(evt);
-   if(evt){
-      var title = '';
-      var content = '';
-      var data = {};
+onesignal.addEventListener("OneSignalNotificationOpened", function(evt) {
+    alert(evt);
+    if (evt) {
+        var title = '';
+        var content = '';
+        var data = {};
 
-      if(evt.title){
-         title = evt.title;
-      }
+        if (evt.title) {
+            title = evt.title;
+        }
 
-      if(evt.body){
-         content = evt.body;
-      }
+        if (evt.body) {
+            content = evt.body;
+        }
 
-      if(evt.additionalData){
-			if(Ti.Platform.osname === 'android'){
-				//Android receives it as a JSON string
-				data = JSON.parse(evt.additionalData);
-			}else{
-				data = evt.additionalData;
-			}
-      }
+        if (evt.additionalData) {
+            if (Ti.Platform.osname === 'android') {
+                //Android receives it as a JSON string
+                data = JSON.parse(evt.additionalData);
+            } else {
+                data = evt.additionalData;
+            }
+        }
 
-      alert("Notification opened! title: " + title + ', content: ' + content + ', data: ' + evt.additionalData);
-   }
+        alert("Notification opened! title: " + title + ', content: ' + content + ', data: ' + evt.additionalData);
+    }
 });
 
-onesignal.addEventListener("OneSignalNotificationReceived",function(evt){
-   alert(' ***** Received! ' + JSON.stringify(evt));
+onesignal.addEventListener("OneSignalNotificationReceived", function(evt) {
+    alert(' ***** Received! ' + JSON.stringify(evt));
 });
 
 win.open();
