53c4e17f8ce1cfb06f2980f1797815051fd2c757
William Rijksen <wrijksen@enrise.com>
2018-03-28 19:44:34 +0200

    add missing fields to notification payload

--- ios/Classes/OneSignalHelper.m
+++ ios/Classes/OneSignalHelper.m
@@ -23,22 +23,12 @@
              @"title": payload.title,
              @"subtitle": payload.subtitle,
              @"body": payload.body,
-             @"launchURL": payload.launchURL
+             @"launchURL": payload.launchURL,
+             @"additionalData": payload.additionalData,
+             @"attachments": payload.attachments,
+             @"actionButtons": payload.actionButtons,
+             @"rawPayload": payload.rawPayload
     };
 }
 
 @end
-//
-///* Additional key value properties set within the payload */
-//@property(readonly)NSDictionary* additionalData;
-//
-///* iOS 10+ : Attachments sent as part of the rich notification */
-//@property(readonly)NSDictionary* attachments;
-//
-///* Action buttons passed */
-//@property(readonly)NSArray *actionButtons;
-//
-///* Holds the original payload received
-// Keep the raw value for users that would like to root the push */
-//@property(readonly)NSDictionary *rawPayload;
-
