794eb55c59b60996ea350c07cb67da40deb3c57c
Carlos Henrique Zinato <carlos@conference-compass.com>
2018-09-26 17:13:07 +0200

    Updates example app.js file

--- example/app.js
+++ example/app.js
@@ -1,95 +1,77 @@
-// open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+    backgroundColor:'white'
 });
 
 var onesignal = require('com.williamrijksen.onesignal');
+onesignal.addEventListener("notificationOpened", onNotificationOpened);
+onesignal.addEventListener("notificationReceived", onNotificationReceived);
 
 var scroll = Ti.UI.createScrollView({
-	layout: 'vertical',
-	top: 50
+    layout: 'vertical',
+    top: 50
 });
 win.add(scroll);
 
+if (Ti.Platform.osname !== 'android') {
+    var startModule = Ti.UI.createButton({
+        title:'Start One Signal module',
+        width:Ti.UI.FILL,
+        height:40,
+        color:'black',
+        opacity:0.8,
+        backgroundColor:'transparent',
+        top:10
+    });
+
+    startModule.addEventListener('click',function(e){
+        onesignal.setInFocusDisplayType('none');
+        onesignal.promptForPushNotificationsWithUserResponse(function(resp){
+            if(resp && resp.accepted){
+                Ti.API.info(new Date() + ': ' + ' One Signal - User accepted iOS push notifications! Thanks =)');
+            }
+        });
+    });
+    scroll.add(startModule);
+}
+
 var addTag = Ti.UI.createButton({
-  title:'Add a tag',
-  width:Ti.UI.FILL,
-  height:40,
-  color:'black',
-  opacity:0.8,
-  backgroundColor:'transparent',
-  top:10
+    title:'Add a tag',
+    width:Ti.UI.FILL,
+    height:40,
+    color:'black',
+    opacity:0.8,
+    backgroundColor:'transparent',
+    top:10
 });
 
 addTag.addEventListener('click',function(e){
-   onesignal.sendTag({key:'tag1', value:true});
-   alert('Tag added');
+    onesignal.sendTag({key:'tag1', value:true});
+    alert('Tag added');
 });
 scroll.add(addTag);
 
 var getTags = Ti.UI.createButton({
-  title:'Get all tags',
-  width:Ti.UI.FILL,
-  height:40,
-  color:'black',
-  opacity:0.8,
-  backgroundColor:'transparent',
-  top:10
+    title:'Get all tags',
+    width:Ti.UI.FILL,
+    height:40,
+    color:'black',
+    opacity:0.8,
+    backgroundColor:'transparent',
+    top:10
 });
 
 getTags.addEventListener('click',function(e){
-	onesignal.getTags(function(e) {
-   	if (!e.success) {
+    onesignal.getTags(function(e) {
+        if (!e.success) {
             alert("Error: " + e.error);
             return
-      }
-      alert(e.results);
-		setTimeout(function () {
-			var tags = JSON.parse(e.results);
-			alert(tags.length);
-		}, 2500);
-   });
+        }
+        alert(e.results);
+    });
 });
 scroll.add(getTags);
 
-var getIds = Ti.UI.createButton({
-  title:'Get player ID',
-  width:Ti.UI.FILL,
-  height:40,
-  color:'black',
-  opacity:0.8,
-  backgroundColor:'transparent',
-  top:10
-});
-
-getIds.addEventListener('click',function(e){
-	onesignal.idsAvailable(function(e) {
-      //pushToken will be nil if the user did not accept push notifications
-		alert(e);
-   });
-});
-scroll.add(getIds);
-
-var postNotificationButton = Ti.UI.createButton({
-  title:'Send a notification',
-  width:Ti.UI.FILL,
-  height:40,
-  color:'black',
-  opacity:0.8,
-  backgroundColor:'transparent',
-  top:10
-});
-
-postNotificationButton.addEventListener('click',function(e){
-	onesignal.postNotification({
-		message:'Titanium test message',
-		playerIds:["00000000-0000-0000-0000-000000000000"]
-	});
-});
-scroll.add(postNotificationButton);
-
-
-onesignal.addEventListener("notificationOpened", function(evt) {
+function onNotificationOpened(evt) {
     alert(evt);
     if (evt) {
         var title = '';
@@ -115,10 +97,9 @@ onesignal.addEventListener("notificationOpened", function(evt) {
 
         alert("Notification opened! title: " + title + ', content: ' + content + ', data: ' + evt.additionalData);
     }
-});
+}
 
-onesignal.addEventListener("notificationReceived", function(evt) {
+function onNotificationReceived(evt) {
     alert(' ***** Received! ' + JSON.stringify(evt));
-});
-
+}
 win.open();
